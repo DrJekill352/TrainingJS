8b634eb8c1de46d0609b93e772cee9a6188e7efb
DrJekill352 <ckam1997@gmail.com>
2017-03-24 16:23:55 +0300

    Refactoring eighth task

--- Task_8/index.js
+++ Task_8/index.js
@@ -1,92 +1,9 @@
-( function () {
-    if(performance.navigation.type == performance.navigation.TYPE_RELOAD){
+(function () {
+    if (performance.navigation.type == performance.navigation.TYPE_RELOAD) {
         localStorage.clear();
-    }else {}
-})();
-
-function getFormInfo() {
-    var _label = document.querySelector(".mp--form-field-label--input").value;
-    var _choisTypeIndex = getSelectOption(".mp--form-choice-type--dropbox");
-    var _chois = new Array();
-    var _inWrap = document.querySelector(".mp--form-checkbox--checkbox").checked;
-
-    var _firstValueRadio = checkRadio(".mp--form-choice--first-field-radio-button")
-    if (_firstValueRadio != false) {
-        _chois.push(_firstValueRadio);
-    }
-
-    var _secondValueRadio = checkRadio(".mp--form-choice--second-field-radio-button")
-    if (_secondValueRadio != false) {
-        _chois.push(_secondValueRadio);
-    }
-
-    var info = new formInfo(_label, _choisTypeIndex, _chois, _inWrap);
-    return info;
-}
-
-function formInfo(label, _choisTypeIndex, chois, inWrap) {
-    this.label = label;
-    this._choisTypeIndex = _choisTypeIndex;
-    this.chois = chois;
-    this.inWrap = inWrap;
-}
-
-function isActiveFild(nameRadio) {
-    return document.querySelector(nameRadio).checked;
-}
-
-function getSelectOption(nameSelect) {
-    var _select = document.querySelector(nameSelect);
-    var _valueSelectOptionIndex = _select.selectedIndex;
-    return _valueSelectOptionIndex;
-}
-
-function checkRadio(nameField) {
-    if (isActiveFild(nameField + " .mp--form-choice--radio-button")) {
-        var _choisValue = document.querySelector(nameField + " .mp--form-choice--text-radio-button").value;
-        return _choisValue;
     } else {
-        return false;
     }
-}
-
-
-function writeStorage() {
-    var _newStorage = new getFormInfo();
-
-    localStorage.setItem("_label", _newStorage.label);
-    localStorage.setItem("_choisTypeIndex", _newStorage._choisTypeIndex);
-    localStorage.setItem("_inWrap", _newStorage.inWrap);
-
-    for (var i = 0; i < _newStorage.chois.length; i++) {
-        localStorage.setItem("_chois_" + i, _newStorage.chois[i]);
-    }
-}
-
-function writeInfoForm() {
-    var _label = document.querySelector(".mp--form-field-label--input").value = localStorage.getItem("_label");
-    if (localStorage.getItem("_inWrap") != "false") {
-        var _inWrap = document.querySelector(".mp--form-checkbox--checkbox").checked = localStorage.getItem("_inWrap");
-    }
-    var _firstValueRadio = localStorage.getItem("_chois_0");
-    if (_firstValueRadio != undefined) {
-        var _firstChoisValue = document.querySelector(".mp--form-choice--first-field-radio-button .mp--form-choice--text-radio-button");
-        _firstChoisValue.value = _firstValueRadio;
-        var _firstRadioButton = document.querySelector(".mp--form-choice--first-field-radio-button .mp--form-choice--radio-button");
-        _firstRadioButton.checked = true;
-    }
-
-    var _secondValueRadio = localStorage.getItem("_chois_1");
-    if (_secondValueRadio != undefined) {
-        var _secondChoisValue = document.querySelector(".mp--form-choice--second-field-radio-button .mp--form-choice--text-radio-button");
-        _secondChoisValue.value = _secondValueRadio;
-        var _secondRadioButton = document.querySelector(".mp--form-choice--second-field-radio-button .mp--form-choice--radio-button");
-        _secondRadioButton.checked = true;
-    }
-
-    var _select = document.querySelector(".mp--form-choice-type--dropbox");
-    _select.selectedIndex = localStorage.getItem("_choisTypeIndex");
-}
+})();
 
 var checkInStorage = (function () {
     if (localStorage.length > 0) {
@@ -97,14 +14,100 @@ var checkInStorage = (function () {
     }
 })();
 
-if (checkInStorage) {
-    try {
-        writeInfoForm();
-    } catch (error) {
-        console.log("Error, Null referens");
+(function () {
+    if (checkInStorage) {
+        try {
+            writeInfoForm();
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    else {
+    }
+})();
+
+function formObjects() {
+    var classNameLabel = ".mp--form-field-label--input";
+    this.label = document.querySelector(classNameLabel);
+
+    var classNameCheckbox = ".mp--form-checkbox--checkbox";
+    this.inWrap = document.querySelector(classNameCheckbox);
+
+    var classNameChoisInput = ".mp--form-choice--text-radio-button";
+    var classNameChoisRadioButton = ".mp--form-choice--radio-button";
+
+    var classNameFirstChois = ".mp--form-choice--first-field-radio-button";
+    this.firstChoisInput = document.querySelector(classNameFirstChois + " " + classNameChoisInput);
+    this.firstRadioButton = document.querySelector(classNameFirstChois + " " + classNameChoisRadioButton);
+
+    var classNameSecondChois = ".mp--form-choice--second-field-radio-button";
+    this.secondChoisInput = document.querySelector(classNameSecondChois + " " + classNameChoisInput);
+    this.secondRadioButton = document.querySelector(classNameSecondChois + " " + classNameChoisRadioButton);
+
+    var classNameChoisType = ".mp--form-choice-type--dropbox";
+    this.choisType = document.querySelector(classNameChoisType);
+}
+
+
+function writeStorage() {
+    var form = new formObjects();
+    var labelValue = form.label.value;
+    var choisTypeIndex = getSelectOption(form.choisType);
+    var inWrap = form.inWrap.checked;
+
+    localStorage.setItem("labelValue", labelValue);
+    localStorage.setItem("choisTypeIndex", choisTypeIndex);
+
+    var isCheckedFirstRadioButton = form.firstRadioButton.checked
+    if (isCheckedFirstRadioButton) {
+        var firstChoisInputValue = form.firstChoisInput.value;
+        localStorage.setItem("firstChoisInputValue", firstChoisInputValue);
+    }
+
+    var isCheckedSecondRadioButton = form.secondRadioButton.checked
+    if (isCheckedSecondRadioButton) {
+        var secondChoisInputValue = form.secondChoisInput.value;
+        localStorage.setItem("secondChoisInputValue", secondChoisInputValue);
+    }
+
+    localStorage.setItem("inWrap", inWrap);
+
+}
+
+function writeInfoForm() {
+    var form = new formObjects();
+
+    var labelValue = localStorage.getItem("labelValue");
+    var choisTypeIndex = localStorage.getItem("choisTypeIndex");
+    var inWrap = localStorage.getItem("inWrap");
+
+    var firstChoisInputValue = localStorage.getItem("firstChoisInputValue");
+    var isCheckedFirstRadioButton = false;
+    if(firstChoisInputValue != undefined){
+        isCheckedFirstRadioButton = true;
+        form.firstChoisInput.value = firstChoisInputValue;
+        form.firstRadioButton.checked = isCheckedFirstRadioButton;
+    }
+
+    var secondChoisInputValue = localStorage.getItem("secondChoisInputValue");
+    var isCheckedSecondRadioButton = false;
+    if(secondChoisInputValue != undefined){
+        isCheckedSecondRadioButton = true;
+        form.secondChoisInput.value = secondChoisInputValue;
+        form.secondRadioButton.checked = isCheckedSecondRadioButton;
+    }
+
+    form.label.value = labelValue;
+    form.choisType.selectedIndex = choisTypeIndex;
+
+    if(inWrap != "false"){
+        form.inWrap.checked = true;
     }
 }
-else {
+
+function getSelectOption(select) {
+    var valueSelectOptionIndex = select.selectedIndex;
+    return valueSelectOptionIndex;
 }
 
 
