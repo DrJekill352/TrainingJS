63a4a1d017d371ccf5a78d0942c43f92bed36c9d
DrJekill352 <ckam1997@gmail.com>
2017-04-02 16:49:58 +0300

    Refactoring ninght task

--- Task_9/index.js
+++ Task_9/index.js
@@ -4,162 +4,12 @@ this.CLASS_NAME_EMPTY_SELL = " calendar--empty-sell";
 this.CLASS_NAME_CURRENT_DAY = " calendar--current-day";
 this.NAME_MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 this.SMALL_NAME_MONTHS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-
-/*
-
-function removeChildren() {
-    var calendar = document.querySelector(".calendar--calendar");
-    var children = calendar.children;
-    var countChildren = children.length;
-    for (var index = 0; index < countChildren; index++) {
-        calendar.removeChild(children[0]);
-    }
-}
-
-
-
-function getWeekElement(arrayDatesWeek) {
-
-}
-
-function viewSelectCalendar() {
-    var calendarDate = getInputDate();
-    var arrayDatesMonth = getArrayDatesMonth(calendarDate);
-    removeChildren();
-    drawCalendar(arrayDatesMonth);
-}
-
-function getInputDate() {
-    var inputDateString = document.querySelector("[name=date]").value;
-    if (inputDateString) {
-        var inputMonthDate = getMonthDate(inputDateString);
-        console.log(inputMonthDate);
-        if (inputMonthDate.year == null || inputMonthDate.month == null) {
-            var newInputDateString = prompt("Error. Invalid input value. Repeat input");
-            inputMonthDate = getMonthDate(newInputDateString);
-        }
-        var calendarDate = new Date(inputMonthDate.year, inputMonthDate.month);
-        return calendarDate;
-    }
-}
-
-function getMonthDate(dateString) {
-
-    var date = dateString.split(' ');
-    var year = null;
-    var month = null;
-    if (date[0].length == 4 && !isNaN(date[0])) {
-        year = date[0];
-        month = getMonthNumber(date[1]);
-    } else {
-        year = date[1];
-        month = getMonthNumber(date[0]);
-    }
-
-    var inputMonthDate = {
-        year: null,
-        monthNumber: null
-    }
-    inputMonthDate.year = year;
-    inputMonthDate.month = month;
-
-    return inputMonthDate;
-}
-
-function getMonthNumber(monthDateString) {
-    for (var index = 0; index < MONTHS.length; index++) {
-        if (MONTHS[index] == monthDateString) {
-            return index;
-        }
-    }
-    for (var index = 0; index < SMALL_MONTHS.length; index++) {
-        if (SMALL_MONTHS[index] == monthDateString) {
-            return index;
-        }
-    }
-    return (+monthDateString - 1);
-}
-
-function viewSelectMonthDate() {
-    var calendarDate = getInputDate();
-    var monthNumber = calendarDate.getMonth();
-    var year = calendarDate.getFullYear();
-    var stringDate = MONTHS[monthNumber] + " " + year;
-    var monthDateElement = document.querySelector(".calendar--month-date");
-    monthDateElement.innerText = stringDate;
-}
-
-function viewNextMonth() {
-    var selectCalendarDate = getInputDate();
-    var nextCalendarDate = new Date(selectCalendarDate.getFullYear(), selectCalendarDate.getMonth() + 1);
-    chengeInputDate(nextCalendarDate);
-    var arrayDatesMonth = getArrayDatesMonth(nextCalendarDate);
-    removeChildren();
-    drawCalendar(arrayDatesMonth);
-}
-
-function viewPreMonth() {
-    var selectCalendarDate = getInputDate();
-    var preCalendarDate = new Date(selectCalendarDate.getFullYear(), selectCalendarDate.getMonth() - 1);
-    chengeInputDate(preCalendarDate);
-    var arrayDatesMonth = getArrayDatesMonth(preCalendarDate);
-    removeChildren();
-    drawCalendar(arrayDatesMonth);
-}
-
-function chengeInputDate(newDate) {
-    var inputDate = document.querySelector("[name=date]");
-    var stringNewDate = newDate.getFullYear() + " " + (newDate.getMonth() + 1);
-    inputDate.value = stringNewDate;
-}
-
-function viewCurrentDate() {
-    var currentDate = new Date();
-    var selectDate = getInputDate();
-    var currentDateElement = null;
-    if (selectDate.getFullYear() == currentDate.getFullYear()) {
-        if (selectDate.getMonth() == currentDate.getMonth()) {
-            var dates = document.querySelectorAll("." + CLASS_NAME_DAY);
-            for (var index = 0; index < dates.length; index++) {
-                if (dates[index].innerText == currentDate.getDate()) {
-                    currentDateElement = dates[index];
-                    break;
-                }
-            }
-        }
-    }
-    if (currentDateElement != null) {
-        currentDateElement.className += CLASS_NAME_CURRENT_DAY;
-    }
-}
-
-
-function showSelectDate(event) {
-    var selectDayElement = event.target;
-    var selectDay = selectDayElement.innerText;
-    var selectMonth = getInputDate();
-    var selectDate = new Date(selectMonth.getFullYear(), selectMonth.getMonth(), selectDay);
-    var stringSelectDate = (selectDate.getMonth() + 1) + "." + selectDate.getDate() + "." + selectDate.getFullYear();
-    console.clear();
-    console.log(stringSelectDate);
-}
-
-function showCalendarInfo() {
-    var monthInfo = document.querySelector(".calendar--month-info");
-    monthInfo.style.display = "flex";
-    var infoRow = document.querySelector(".calendar--info-row");
-    infoRow.style.display = "flex";
-}
-
-document.querySelector(".calendar").addEventListener("click", viewCurrentDate);
-document.querySelector(".calendar--calendar").addEventListener("click", showSelectDate);
-document.querySelector("[name=button]").addEventListener("click", viewSelectCalendar);
-document.querySelector("[name=button]").addEventListener("click", viewSelectMonthDate);
-document.querySelector("[name=button]").addEventListener("click", showCalendarInfo);
-document.querySelector(".calendar--preMonth-button").addEventListener("click", viewPreMonth);
-document.querySelector(".calendar--preMonth-button").addEventListener("click", viewSelectMonthDate);
-document.querySelector(".calendar--nextMonth-button").addEventListener("click", viewNextMonth);
-document.querySelector(".calendar--nextMonth-button").addEventListener("click", viewSelectMonthDate);*/
+this.CALENDAR_DOM_ELEMENT = document.querySelector(".calendar--calendar");
+this.INPUT_MONTH_DATE_DOM_ELEMENT = document.querySelector("[name=date]");
+this.SHOW_CALENDAR_BUTTON_DATE_DOM_ELEMENT = document.querySelector("[name=button]");
+this.SHOW_NEXT_MONTH_DOM_ELEMENT = document.querySelector(".calendar--nextMonth-button");
+this.SHOW_PRE_MONTH_DOM_ELEMENT = document.querySelector(".calendar--preMonth-button");
+this.MONTH_DATE_DOM_ELEMENT = document.querySelector(".calendar--month-date");
 
 function Calendar() {
     this.getMonthDates = function (selectMonthDate) {
@@ -167,7 +17,7 @@ function Calendar() {
         var arrayMonthDates = [];
 
         for (var day = 1; day <= selectMonthLastDateNumber; day++) {
-            monthDay = new Date(monthCurrentDate.getFullYear(), monthCurrentDate.getMonth(), day);
+            monthDay = new Date(selectMonthDate.getFullYear(), selectMonthDate.getMonth(), day);
             arrayMonthDates.push(monthDay);
         }
 
@@ -195,52 +45,84 @@ function Calendar() {
 
 function CalendarStrategy() {
 
+    var calendar = new Calendar();
+
     var showInputMonth = function () {
-
-    }
-
-    var getInputMonthDate = function () {
-
+        var inputDate = getInputMonthDate();
+        var monthDates = calendar.getMonthDates(inputDate);
+        removeCalendarChildren();
+        drawSelectMonth(monthDates);
     }
 
     var showNextInputMonth = function () {
-
+        var inputDate = getInputMonthDate();
+        var nextMonthDate = new Date(inputDate.getFullYear(), inputDate.getMonth() + 1);
+        chengeInputDate(nextMonthDate);
+        var monthDates = calendar.getMonthDates(nextMonthDate);
+        removeCalendarChildren();
+        drawSelectMonth(monthDates);
     }
 
     var showPreInputMonth = function () {
-
+        var inputDate = getInputMonthDate();
+        var preMonthDate = new Date(inputDate.getFullYear(), inputDate.getMonth() - 1);
+        chengeInputDate(preMonthDate);
+        var monthDates = calendar.getMonthDates(preMonthDate);
+        removeCalendarChildren();
+        drawSelectMonth(monthDates);
     }
 
-    var drawSelectMonth = function (arraySelectMonthDates) {
-        var arrayWeeks = [];
-        arrayWeeks.push(getArrayFirstWeekDates(arraySelectMonthDates));
-        while (true) {
-            var lastWeek = arrayWeeks[arrayWeeks.length - 1];
-            var lastdateLastWeek = lastWeek[lastWeek.length - 1];
-            if (lastdateLastWeek == undefined) {
-                break;
+    var chengeInputDate = function (newDate) {
+        var stringNewDate = newDate.getFullYear() + " " + (newDate.getMonth() + 1);
+        INPUT_MONTH_DATE_DOM_ELEMENT.value = stringNewDate;
+    }
+
+    var removeCalendarChildren = function () {
+        var calendarChildren = CALENDAR_DOM_ELEMENT.children;
+        var countCalendarChildren = calendarChildren.length;
+        for (var index = 0; index < countCalendarChildren; index++) {
+            CALENDAR_DOM_ELEMENT.removeChild(calendarChildren[0]);
+        }
+    }
+
+    var drawSelectMonth = function (monthDates) {
+        var weeks = getMonthWeeks(monthDates);
+
+        for (var index = 0; index < weeks.length; index++) {
+            var weekDomElement = getWeekDomElement(weeks[index])
+            CALENDAR_DOM_ELEMENT.appendChild(weekDomElement);
+        }
+    }
+
+    var getMonthWeeks = function (monthDates) {
+        var weeks = [];
+        var firstWeekDates = getFirstWeekDates(monthDates);
+        weeks.push(firstWeekDates);
+
+        var isUndefined = false;
+        while (!isUndefined) {
+            var lastWeek = weeks[weeks.length - 1];
+            var lastWeekLastDate = lastWeek[lastWeek.length - 1];
+            if (lastWeekLastDate == undefined) {
+                isUndefined = true;
             } else {
-                arrayWeeks.push(getArrayWeekDates(arraySelectMonthDates, lastdateLastWeek.getDate()));
+                weeks.push(getWeekDates(monthDates, lastWeekLastDate.getDate()));
             }
         }
-        var calendar = document.querySelector(".calendar--calendar");
-
-        for (var index = 0; index < arrayWeeks.length; index++) {
-            calendar.appendChild(getWeekElement(arrayWeeks[index]));
-        }
+        return weeks;
     }
 
-    function getFirstWeekDates(monthDates) {
-        var monthFirstDayDate = monthDates[0].getDay();
-        var monthEndFirstWeekDate;
+    var getFirstWeekDates = function (monthDates) {
+        var newMonthDates = monthDates.slice();
+        var monthFirstDayDate = newMonthDates[0].getDay();
+        var firstWeekLastDayDate = getFirstWeekLastDayDate(newMonthDates);
 
-        var arrayFirstWeekDates = [];
-        var iterator = 0;
+        var firstWeekDates = [];
         for (var index = monthFirstDayDate; index < 7; index++) {
-            arrayFirstWeekDates[index] = arrayDatesMonth[iterator];
-            iterator++;
+            firstWeekDates[index] = newMonthDates.shift();
         }
-        return arrayFirstWeekDates;
+
+        return firstWeekDates;
     }
 
     var getFirstWeekLastDayDate = function (monthDates) {
@@ -254,22 +136,23 @@ function CalendarStrategy() {
         return weekLastDayDate;
     }
 
-    function getArrayWeekDates(arrayDatesMonth, startWeekDate) {
-        var arrayWeekDates = [];
-        var currentWeekDate = startWeekDate;
-        var lenghtWeek = 7;
-        for (var index = 0; index < lenghtWeek; index++) {
-            arrayWeekDates[index] = arrayDatesMonth[currentWeekDate];
-            currentWeekDate++;
+    var getWeekDates = function (monthDates, weekStartDate) {
+        var weekDates = [];
+        var weekLenght = 7;
+
+        for (var currentWeekDate = weekStartDate; currentWeekDate < weekStartDate + weekLenght; currentWeekDate++) {
+            var date = monthDates[currentWeekDate];
+            weekDates.push(date);
         }
-        return arrayWeekDates;
+
+        return weekDates;
     }
 
     var getWeekDomElement = function (weekDays) {
         var weekDomElement = document.createElement('div');
         weekDomElement.className = CLASS_NAME_WEEK;
         for (var index = 0; index < weekDays.length; index++) {
-            var dayDomElement = getDayElement(weekDays[index]);
+            var dayDomElement = getDayDomElement(weekDays[index]);
             weekDomElement.appendChild(dayDomElement);
         }
 
@@ -280,11 +163,124 @@ function CalendarStrategy() {
         var dayDomElement = document.createElement('div');
         dayDomElement.className = CLASS_NAME_DAY;
         if (dayNumber != undefined) {
-            dayDomElement.innerHTML = dayDomElement.getDate();
+            dayDomElement.innerHTML = dayNumber.getDate();
         } else {
             dayDomElement.innerHTML = "";
             dayDomElement.className += CLASS_NAME_EMPTY_SELL;
         }
         return dayDomElement;
     }
+
+    var getInputMonthDate = function () {
+        var inputMonthDateStringValue = INPUT_MONTH_DATE_DOM_ELEMENT.value;
+
+        if (inputMonthDateStringValue) {
+            var inputMonthDateValue = getMonthDate(inputMonthDateStringValue);
+
+            if (inputMonthDateValue.year == null || inputMonthDateValue.month == null) {
+                var inputMonthDateStringValue = prompt("Error. Invalid input value. Repeat input");
+                inputMonthDateValue = getMonthDate(inputMonthDateStringValue);
+            }
+
+            var inputMonthDate = new Date(inputMonthDateValue.year, inputMonthDateValue.month);
+            return inputMonthDate;
+        }
+    }
+
+    var getMonthDate = function (dateString) {
+
+        var date = dateString.split(' ');
+        var year = null;
+        var month = null;
+        if (date[0].length == 4 && !isNaN(date[0])) {
+            year = date[0];
+            month = getMonthNumber(date[1]);
+        } else {
+            year = date[1];
+            month = getMonthNumber(date[0]);
+        }
+
+        var inputMonthDate = {
+            year: null,
+            month: null
+        }
+        inputMonthDate.year = year;
+        inputMonthDate.month = month;
+
+        return inputMonthDate;
+    }
+
+    var getMonthNumber = function (monthString) {
+        for (var index = 0; index < NAME_MONTHS.length; index++) {
+            if (NAME_MONTHS[index] == monthString) {
+                return index;
+            }
+        }
+        for (var index = 0; index < SMALL_NAME_MONTHS.length; index++) {
+            if (SMALL_NAME_MONTHS[index] == monthString) {
+                return index;
+            }
+        }
+        return (+monthString - 1);
+    }
+
+    var showSelectMonthDate = function () {
+        var calendarDate = getInputMonthDate();
+        var monthNumber = calendarDate.getMonth();
+        var year = calendarDate.getFullYear();
+        var stringDate = NAME_MONTHS[monthNumber] + " " + year;
+
+        MONTH_DATE_DOM_ELEMENT.innerText = stringDate;
+    }
+
+    var showCalendarInfo = function () {
+        var monthInfo = document.querySelector(".calendar--month-info");
+        monthInfo.style.display = "flex";
+        var infoRow = document.querySelector(".calendar--info-row");
+        infoRow.style.display = "flex";
+    }
+
+    var selectDate = function (event) {
+        var selectDayDomElement = event.target;
+        var selectDay = selectDayElement.innerText;
+        var selectMonth = getInputMonthDate();
+        var selectDayDate = new Date(selectMonth.getFullYear(), selectMonth.getMonth(), selectDay);
+        calendar.setSelectDayDate(selectDayDate);
+    }
+
+    var viewCurrentDate = function () {
+        var currentDayDate = calendar.getCurrentDayDate();
+        var selectDate = getInputMonthDate();
+        var currentDateDomElement = null;
+        if (selectDate.getFullYear() == currentDayDate.getFullYear()) {
+            if (selectDate.getMonth() == currentDayDate.getMonth()) {
+                var datesDomElements = document.querySelectorAll("." + CLASS_NAME_DAY);
+                for (var index = 0; index < datesDomElements.length; index++) {
+                    if (datesDomElements[index].innerText == currentDayDate.getDate()) {
+                        currentDateDomElement = datesDomElements[index];
+                        break;
+                    }
+                }
+            }
+        }
+        if (currentDateDomElement != null) {
+            currentDateDomElement.className += CLASS_NAME_CURRENT_DAY;
+        }
+    }
+
+    document.querySelector(".calendar").addEventListener("click", viewCurrentDate);
+    CALENDAR_DOM_ELEMENT.addEventListener("click", selectDate);
+    SHOW_CALENDAR_BUTTON_DATE_DOM_ELEMENT.addEventListener("click", showInputMonth);
+    SHOW_CALENDAR_BUTTON_DATE_DOM_ELEMENT.addEventListener("click", showSelectMonthDate);
+    SHOW_CALENDAR_BUTTON_DATE_DOM_ELEMENT.addEventListener("click", showCalendarInfo);
+    SHOW_PRE_MONTH_DOM_ELEMENT.addEventListener("click", showPreInputMonth);
+    SHOW_PRE_MONTH_DOM_ELEMENT.addEventListener("click", showSelectMonthDate);
+    SHOW_NEXT_MONTH_DOM_ELEMENT.addEventListener("click", showNextInputMonth);
+    SHOW_NEXT_MONTH_DOM_ELEMENT.addEventListener("click", showSelectMonthDate);
 }
+
+var CalendarRendar = {
+    
+}
+
+var calendarStrategy = new CalendarStrategy();
